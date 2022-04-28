0566371df884867ad6033e474095d9b672b1cad8
shayforde <shayforde@gmail.com>
2022-04-28 21:45:38 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+   <NavBar />
+
+    <RouterView />
+</div>
+
+</template>
+
+<script setup>
+
+import NavBar from '@/components/Layout/NavBar.vue';
+</script>
+<style>
+@import 'bulma/css/bulma.min.css'
+</style>
+
---
+++ src/views/ViewNotes.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="notes">
+        <h1>Notes</h1>
+    </div>
+        
+    
+</template>
